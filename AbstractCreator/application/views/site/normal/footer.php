ca6f35ab57f196e37715050a3e2f50090883ada0
Matt Carter <m@ttcarter.com>
2013-02-25 13:38:20 +1000

    Applied Wintertide theme to project

--- AbstractCreator/application/views/site/normal/footer.php
+++ AbstractCreator/application/views/site/normal/footer.php
@@ -1,6 +1,8 @@
-					</div>
-				</div>
-			</div>
-		</section>
-	</body>
+		</div>
+	</div>
+	<footer>
+		<hr>
+		<p>&copy; <?=date('Y')?> <a href="http://www.crebp.net.au/">CREBP</a></p>
+	</footer>
+  </body>
 </html>
--- AbstractCreator/application/views/site/normal/header.php
+++ AbstractCreator/application/views/site/normal/header.php
@@ -3,47 +3,63 @@
 <? include('application/views/site/head.php') ?>
 
 <body>
-<div class="header container-fluid">
-	<div class="row-fluid">
-		<div class="span10 offset1">
-			<div class="logo">
-				<a href="/"><h1><?=SITE_TITLE?></h1></a>
-			</div>
-			<div class="search">
-				<input id="search" class="input-xlarge focused" type="text" value="search" placeholder="Search">
-			</div>
-		</div>
-	</div>
-</div>
-<div class="user-box container-fluid">
-	<div class="row-fluid">
-		<div class="span10 offset1">
-			<div class="user-content">
-				<div class="row-fluid">
-					<div class="top-user-info span9">
-						<ul class="user-config">
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-									<div class="welcome">Welcome, <?=isset($_SESSION['user']['firstname']) ? ucfirst($_SESSION['user']['firstname']) : 'Admin'?><b class="caret"></b></div>
-								</a>
-								<ul class="dropdown-menu">
-									<li><a href="/logout"><i class="icon-off"></i> Logout</a></li>
-								</ul>
-							</li>
+	<div class="navbar">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<ul class="nav pull-right">
+					<li id="fat-menu" class="dropdown">
+						<a href="#" id="drop3" role="button" class="dropdown-toggle" data-toggle="dropdown">
+							<i class="icon-user"></i> Jack Smith
+							<i class="icon-caret-down"></i>
+						</a>
+
+						<ul class="dropdown-menu">
+							<li><a tabindex="-1" href="#">Settings</a></li>
+							<li class="divider"></li>
+							<li><a tabindex="-1" href="sign-in.html">Logout</a></li>
 						</ul>
-						<!-- <span class="badge badge-important">1 new message</span> <span class="badge badge-info">8 messages</span> <span class=" badge badge-success">15 followers</span> -->
-					</div>
-				</div>
+					</li>
+				</ul>
+				<a class="brand" href="/"><span class="first">Abstract</span> <span class="second">Creator</span></a>
 			</div>
 		</div>
 	</div>
-</div>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span3">
+				<div class="sidebar-nav">
+				  <div class="nav-header" data-toggle="collapse" data-target="#dashboard-menu"><i class="icon-dashboard"></i>Dashboard</div>
+					<ul id="dashboard-menu" class="nav nav-list collapse in">
+						<li><a href="index.html">Home</a></li>
+						<li ><a href="users.html">Sample List</a></li>
+						<li ><a href="user.html">Sample Item</a></li>
+						<li ><a href="gallery.html">Gallery</a></li>
+						<li ><a href="calendar.html">Calendar</a></li>
+						<li ><a href="faq.html">Faq</a></li>
+						<li ><a href="help.html">Help</a></li>
+						
+					</ul>
+				<div class="nav-header" data-toggle="collapse" data-target="#accounts-menu"><i class="icon-briefcase"></i>Account<span class="label label-info">+10</span></div>
+				<ul id="accounts-menu" class="nav nav-list collapse in">
+				  <li ><a href="sign-in.html">Sign In</a></li>
+				  <li ><a href="sign-up.html">Sign Up</a></li>
+				  <li ><a href="reset-password.html">Reset Password</a></li>
+				</ul>
 
-<section class="container-fluid">
-	<div class="row-fluid">
-		<div class="span10 offset1">
-			<div class="row-fluid">
-			<? include('application/views/site/sidebar.php') ?>
+				<div class="nav-header" data-toggle="collapse" data-target="#settings-menu"><i class="icon-exclamation-sign"></i>Error Pages</div>
+				<ul id="settings-menu" class="nav nav-list collapse in">
+				  <li ><a href="403.html">403 page</a></li>
+				  <li ><a href="404.html">404 page</a></li>
+				  <li ><a href="500.html">500 page</a></li>
+				  <li ><a href="503.html">503 page</a></li>
+				</ul>
 
-			<div class="span9">
-				<? include('application/views/site/messages.php') ?>
+				<div class="nav-header" data-toggle="collapse" data-target="#legal-menu"><i class="icon-legal"></i>Legal</div>
+				<ul id="legal-menu" class="nav nav-list collapse in">
+				  <li ><a href="privacy-policy.html">Privacy Policy</a></li>
+				  <li ><a href="terms-and-conditions.html">Terms and Conditions</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="span9">
+			<? include('application/views/site/messages.php') ?>
